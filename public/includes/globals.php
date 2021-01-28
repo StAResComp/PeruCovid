54997bff9f46e42b4b58ff1a4d02937fdfb0f2db
Swithun Crowe <cs2@st-andrews.ac.uk>
2021-01-28 15:07:06 +0000

    stop ini file being exposed via website

--- public/includes/globals.php
+++ public/includes/globals.php
@@ -8,7 +8,7 @@ require_once 'autoload.php';
 
 define('BASE_PATH', __DIR__ . '/../'); // points to public directory
 define('RESPONSE_DIR', '/home/perucovid/responses/');
-define('INI_FILE', 'errors.ini');
+define('INI_FILE', 'errors.ini.php');
 define('API_KEY_FILE', '/home/perucovid/apikey.txt');
 
 ?>
